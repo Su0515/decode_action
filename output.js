96db701473e5062ef349db71b304c30126c3df5c
action <action@github.com>
2024-07-01 08:05:52 +0000

    Add decoded output file

---
+++ output.js
@@ -0,0 +1,8 @@
+//Mon Jul 01 2024 08:05:52 GMT+0000 (Coordinated Universal Time)
+//Base:https://github.com/echo094/decode-js
+//Modify:https://github.com/smallfawn/decode_action
+(function (c, d) {
+  c.update = "2019年09月12日15:24:29更新";
+  d.info = "这个是一个本站对JavaScript 脚本的一个最牛加密，兼容性适中，解密难度极大";
+  d.feedback = "有问题请联系QQ 84034666";
+})(window, document);
